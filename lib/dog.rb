21d6dba56847b76530fc2a0ace4dfe8635ec71d2
John Carter <johncarter352@gmail.com>
2020-04-08 04:41:07 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(dog_name)
+    @the_dogs_name = dog_name
+  end
+  
+  def name
+    @the_dogs_name
+  end
+  
+  def breed=(dog_breed)
+    @the_dogs_breed = dog_breed
+  end
+  
+  def breed
+    @the_dogs_breed
+  end
+end
